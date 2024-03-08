7ad68872110623a2937e7cd7297337145ccdb433
imtiyazaehmad <imtiyaz@vspl.com>
2024-03-08 09:38:38 +0530

    url update

--- api/index.js
+++ api/index.js
@@ -10,10 +10,7 @@ app.use(bodyParser.json({ type: 'application/*+json' }));
 
 app.use(cors());
 
-mongoose.connect('mongodb+srv://adimi0125:adimi0125@clustor0.uinwwnr.mongodb.net/?retryWrites=true&w=majority&appName=clustor0/login', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://adimi0125:adimi0125@clustor0.uinwwnr.mongodb.net/?retryWrites=true&w=majority&appName=clustor0/login');
 app.use('/v1',router);
 //app.get('/',(req, res) => res.send("Express on vercel app"));
 
