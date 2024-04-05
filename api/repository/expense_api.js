77877c5f2f838afbd4e1a1956339f5eefa4c34a3
imtiyazaehmad <imtiyaz@vspl.com>
2024-04-05 16:42:05 +0530

    - testing add expense.

--- api/repository/expense_api.js
+++ api/repository/expense_api.js
@@ -6,23 +6,64 @@ const  FamilyMemberModel = require('../../models/family_member_model');
 //Add Expesne
 exports.addExpense = async (req, res) => {
   try {
-    var  expenseModel = req.body;
+    var expenseModel = req.body;
     console.log('Request Body:', expenseModel); // Log the request body
     // Add expense entry
     const newExpense = await ExpenseModel.create(expenseModel);
     await newExpense.save();
-       
+    
     // Update wallet balance
     const { memberId, remainingWalletBalance } = expenseModel;
     await FamilyMemberModel.updateOne(
       { _id: memberId },
       { $set: { walletBalance: remainingWalletBalance } }
     );
-    console.log('newExpense:', newExpense); // Log the request body
-    res.status(200).json(ResponseModel.success(newExpense, StringConstants.EXPENSE_ADDED_SUCCESSFULLY));
+    // Construct the response JSON with specific data
+    const responseData = {
+      status: 200,
+      success: true,
+      data: {
+        createdDate: newExpense.createdDate, // Assuming newExpense has a createdDate property
+        name: newExpense.name,
+        email: newExpense.email,
+        familyCode: newExpense.familyCode,
+        familyCodeId: newExpense.familyCodeId,
+        role: newExpense.role,
+        memberId: newExpense.memberId,
+        description: newExpense.description,
+        amount: newExpense.amount,
+        remainingWalletBalance: newExpense.remainingWalletBalance,
+        category: newExpense.category,
+        date: newExpense.date,
+        id: newExpense._id // Assuming newExpense has an _id property for the ID
+      },
+      message: StringConstants.EXPENSE_ADDED_SUCCESSFULLY
+    };
+    res.status(200).json(responseData);
   } catch (error) {
     res.status(500).json({ error: error.message });
-  } };
+  }
+};
+
+// exports.addExpense = async (req, res) => {
+//   try {
+//     var  expenseModel = req.body;
+//     console.log('Request Body:', expenseModel); // Log the request body
+//     // Add expense entry
+//     const newExpense = await ExpenseModel.create(expenseModel);
+//     await newExpense.save();
+       
+//     // Update wallet balance
+//     const { memberId, remainingWalletBalance } = expenseModel;
+//     await FamilyMemberModel.updateOne(
+//       { _id: memberId },
+//       { $set: { walletBalance: remainingWalletBalance } }
+//     );
+//     console.log('newExpense:', newExpense); // Log the request body
+//     res.status(200).json(ResponseModel.success(newExpense, StringConstants.EXPENSE_ADDED_SUCCESSFULLY));
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   } };
 
 //Get Expense Data
 exports.getExpenseData =  async (req, res) => {
