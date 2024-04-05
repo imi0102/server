a40afe6b94dc83920d81fa1923b4a30714c4a2fa
imi0102 <imidev01+github@gmail.com>
2024-04-05 23:01:15 +0530

    testing

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require('../router.js');
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 5000;
-var bodyParser = require('body-parser')
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
-app.use(cors());
+//app.use(cors());
 
 mongoose.connect('mongodb+srv://adimi0125:adimi0125@clustor0.uinwwnr.mongodb.net/?retryWrites=true&w=majority&appName=clustor0/login');
 app.use('/v1',router);
